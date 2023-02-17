4aea4b910e425e75942b8839eac92bd0147ac0a3
Dupsyn <awemodupejohnson@gmail.com>
2023-02-16 20:22:41 -0800

    se eng

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#include <stduo.h>
+#include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-
+  *main - Entry point
+  *return: always 0 (success)
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
